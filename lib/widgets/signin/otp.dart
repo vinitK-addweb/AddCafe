8c49fa27ef8d5704cc6d7a316144d313899d8f46
vinit <vinit4d@gmail.com>
2022-10-03 12:59:59 +0530

    solve some bugs

--- lib/widgets/signin/otp.dart
+++ lib/widgets/signin/otp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/services.dart';
+import './signin.dart';
 
 class Otp extends StatelessWidget {
   const Otp({super.key});
@@ -8,7 +9,8 @@ class Otp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+        body: SingleChildScrollView(
+      child: Container(
         // margin: EdgeInsets.only(top: 100),
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -16,22 +18,27 @@ class Otp extends StatelessWidget {
             Container(
               margin: EdgeInsets.symmetric(vertical: 60),
               width: double.infinity,
-              // height: 100,
+              height: 50,
               child: Stack(children: [
                 Container(
-                  margin: EdgeInsets.symmetric(horizontal: 100),
+                  margin: EdgeInsets.symmetric(horizontal: 100, vertical: 5),
                   child: Text(
                     'Otp Verification',
                     style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                   ),
                 ),
                 Positioned(
-                  child: Icon(
-                    Icons.arrow_back_ios_new,
-                    size: 30,
-                  ),
-                  left: 5,
-                ),
+                    left: 5,
+                    child: MaterialButton(
+                      child: Icon(
+                        Icons.arrow_back_ios_new,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Mylogin()));
+                      },
+                    )),
               ]),
             ),
             Column(
@@ -163,7 +170,7 @@ class Otp extends StatelessWidget {
                       },
                       style: Theme.of(context).textTheme.headline6,
                       textAlign: TextAlign.center,
-                      keyboardType: TextInputType.name,
+                      keyboardType: TextInputType.number,
                       inputFormatters: [
                         LengthLimitingTextInputFormatter(1),
                         FilteringTextInputFormatter.digitsOnly
@@ -197,7 +204,7 @@ class Otp extends StatelessWidget {
               ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
                     margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
@@ -244,6 +251,6 @@ class Otp extends StatelessWidget {
           ],
         ),
       ),
-    );
+    ));
   }
 }
