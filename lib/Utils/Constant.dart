3a4e0389404d8f6d47d2e3bcc7e36d71cd9ec63d
vinit <vinit@addwebsolution.in>
2022-11-24 13:41:52 +0530

    solve the merge conflicts

---
+++ lib/Utils/Constant.dart
@@ -0,0 +1,11 @@
+String kBaseUrl = 'https://cafe.addwebprojects.com/api/v1/';
+String kImgUrl = 'https://cafe.addwebprojects.com';
+// String kBaseUrl = 'http://localhost:3000/';
+
+const kASSETURL = 'assets/image/';
+const kDefaultImage = 'mainlogo.png';
+const kTOKEN = 'token';
+String kTOKENSAVED = '';
+
+const kUSERID = '_id';
+String kUSERIDSAVED = '';
